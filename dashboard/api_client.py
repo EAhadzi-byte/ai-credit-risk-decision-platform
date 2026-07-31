1c8bbc5ab9e6208c403a5835f7f08294b85b8a86
Emmanuel  Saka Ahadzi <emmasakaahadzi24@gmail.com>
2026-07-31 20:01:48 +0100

    Update api_client.py

--- dashboard/api_client.py
+++ dashboard/api_client.py
@@ -1,30 +1,50 @@
 """
-=========================================================
 Credit Risk AI Platform
 API Client
-=========================================================
+___________________________________________________
 """
 
 import requests
-
+from app.schemas import LoanApplication
+from app.services import prediction_service
+from app.services import explanation_service
 
 API_URL = "http://127.0.0.1:8000"
+
+# Check whether FastAPI is running
+# _________________________________________________
+
+def api_available():
+
+    try:
+
+        response = requests.get(f"{API_URL}/health",timeout=1)
+
+        return response.status_code == 200
+
+    except:
+
+        return False
 
 
 # PREDICTION REQUEST
 # _____________________________________________________
 
 def predict_loan(application):
+    
+    if api_available():
 
-    response = requests.post(
+        response = requests.post(f"{API_URL}/predict",json=application)
 
-        f"{API_URL}/predict",
+        response.raise_for_status()
 
-        json=application)
+        return response.json()
+        
+    print("FastAPI not available. Using local prediction service...")
 
-    response.raise_for_status()
+    application = LoanApplication(**application)
 
-    return response.json()
+    return prediction_service(application)
 
 
 # EXPLANATION REQUEST
@@ -32,12 +52,17 @@ def predict_loan(application):
 
 def explain_loan(application):
 
-    response = requests.post(
+    if api_available():
 
-        f"{API_URL}/explain",
+        response = requests.post(f"{API_URL}/explain",json=application)
 
-        json=application)
+        response.raise_for_status()
 
-    response.raise_for_status()
+        return response.json()
 
-    return response.json()
+    print("FastAPI not available. Using local explanation service...")
+
+    application = LoanApplication(**application)
+
+    return explanation_service(application)
+    
